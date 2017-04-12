fded459e749fcbc946b8fbd700294b8a74a26c17
Kayla Galway <kaylagalway@Kaylas-MacBook-Pro.local>
2017-04-12 13:55:01 -0400

    first commit

---
+++ coreImageFilterCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreImageFilterCollection
+//
+//  Created by Kayla Galway on 4/12/17.
+//  Copyright © 2017 Kayla Galway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreImageFilterCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreImageFilterCollection
+//
+//  Created by Kayla Galway on 4/12/17.
+//  Copyright © 2017 Kayla Galway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
